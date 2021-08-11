4d09817608224b2c09e35d3ec901881dd18a9efa
Kladov Sergei <sergey1208ser@gmail.com>
2021-08-11 13:52:27 +0700

    updates

---
+++ main.js
@@ -0,0 +1,54 @@
+const workMenu = document.getElementById("work-menu");
+const hobbyMenu = document.getElementById("hobby-menu");
+const hobbyExpand = document.getElementById("hobby-expand");
+const workExpand = document.getElementById("work-expand");
+const workBotton = document.getElementById("CVstuff");
+const hobbyBotton = document.getElementById("hobby");
+
+workBotton.addEventListener('mouseenter', function(){
+    if(workMenu.style.display=='none'){
+        workMenu.style.display='flex';
+    }
+})
+
+workBotton.addEventListener('mouseleave', function(){
+    if(workMenu.style.display!='none'){
+        workMenu.style.display='none';
+    }
+})
+
+workMenu.addEventListener('mouseenter', function(){
+    if(workMenu.style.display=='none'){
+        workMenu.style.display='flex';
+    }
+})
+
+workMenu.addEventListener('mouseleave', function(){
+    if(workMenu.style.display!='none'){
+        workMenu.style.display='none';
+    }
+})
+
+hobbyBotton.addEventListener('mouseenter', function(){
+    if(hobbyMenu.style.display=='none'){
+        hobbyMenu.style.display='flex';
+    }
+})
+
+hobbyBotton.addEventListener('mouseleave', function(){
+    if(hobbyMenu.style.display!='none'){
+        hobbyMenu.style.display='none';
+    }
+})
+
+hobbyMenu.addEventListener('mouseenter', function(){
+    if(hobbyMenu.style.display=='none'){
+        hobbyMenu.style.display='flex';
+    }
+})
+
+hobbyMenu.addEventListener('mouseleave', function(){
+    if(hobbyMenu.style.display!='none'){
+        hobbyMenu.style.display='none';
+    }
+})
