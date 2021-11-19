e63ee54e2be7d84f62a51d5b493f70c344ce9283
Kladov Sergei <sergey1208ser@gmail.com>
2021-11-19 18:41:38 +0700

    trying to execute a command from javaScript

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 var path = window.location.pathname;
 var page = path.split("/").pop();
 
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+$.getJSON('https://api.ipify.org?format=json', function(data){
+    console.log(data.ip);
+});
+
 //SLIDE-OUT MENUS
 
 const workMenu = document.getElementById("work-menu");
